fb0e22cbd6a747cf4e364ec09190b179778267a4
Hadas Farhi <hadasfa@monday.com>
2022-11-20 17:15:40 +0200

    wip

--- src/components/Dropdown/components/Control/Control.jsx
+++ src/components/Dropdown/components/Control/Control.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { components } from "react-select";
 
 const Control = props => {
-  const { selectProps } = props;
+  const { controlRef } = props;
   const control = <components.Control {...props} />;
-  const controlRef = selectProps?.selectProps?.controlRef;
   if (controlRef)
     return (
       <div className="monday-dropdown_scrollable-wrapper" ref={controlRef}>
